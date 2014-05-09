637035b3dceca62d11d03636a1ead54300ebfdd3
lava-hammer <lava_hammer@163.com>
2014-05-09 16:14:04 +0800

    new table

--- Resources/table/bounty.js
+++ Resources/table/bounty.js
@@ -7,7 +7,7 @@ exports.data = [
         "date":{
             "segment":[
                 {
-                    "start":"9:00",
+                    "start":"12:00",
                     "end":"23:59"
                 }
             ]
@@ -15,114 +15,11 @@ exports.data = [
         "begin":1,
         "titlePic":"bounty-icon3.png",
         "timePic":"bounty-time3.png",
-        "notify":1,
-        "notifyButton":"马上出征",
-        "notifyText":"哥布林中的土豪出现了！趁他们逃跑之前抢他们的钱！",
+	"notify":1,
+        "notifyButton":"这就拿下",
+        "notifyText":"报⋯⋯！发现一群哥布林土豪，请老大立刻拿下它们！",
          "level":[
             {
-                "powerLimit":500,
-                "stage":114,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-
-            },
-            {
-                "powerLimit":1000,
-                "stage":115,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-            },
-            {
-                "powerLimit":3000,
-                "stage":116,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "bountyId":1,
-        "title":"捉拿土豪哥布林4",
-        "desc":"哥布林中的土豪出现了！趁他们逃跑之前击倒，获得大量金币！",
-        "count":3,
-        "date":{
-            "segment":[
-                {
-                    "start":"12:00",
-                    "end":"23:59"
-                }
-            ]
-        },
-        "begin":1,
-        "titlePic":"bounty-icon3.png",
-        "timePic":"bounty-time3.png",
-        "level":[
-            {
-                "powerLimit":100,
-                "stage":114,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-
-            },
-            {
-                "powerLimit":100,
-                "stage":115,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-            },
-            {
-                "powerLimit":100,
-                "stage":116,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-            },
-            {
-                "powerLimit":100,
-                "stage":116,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        "bountyId":2,
-        "title":"捉拿土豪哥布林5",
-        "desc":"哥布林中的土豪出现了！趁他们逃跑之前击倒，获得大量金币！",
-        "count":3,
-        "date":{
-            "segment":[
-                {
-                    "start":"12:00",
-                    "end":"23:59"
-                }
-            ]
-        },
-        "begin":1,
-        "titlePic":"bounty-icon3.png",
-        "timePic":"bounty-time3.png",
-        "level":[
-            {
                 "powerLimit":100,
                 "stage":114,
                 "prize":[
@@ -143,24 +40,6 @@ exports.data = [
             },
             {
                 "powerLimit":3000,
-                "stage":116,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-            },
-            {
-                "powerLimit":4000,
-                "stage":116,
-                "prize":[
-                    {
-                        "type":1
-                    }
-                ]
-            },
-            {
-                "powerLimit":5000,
                 "stage":116,
                 "prize":[
                     {
