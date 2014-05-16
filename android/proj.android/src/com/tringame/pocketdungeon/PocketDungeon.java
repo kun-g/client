d4d8960a4f6a0a81c118fb0be55bd2364d2058b0
lava-hammer <lava_hammer@163.com>
2014-05-16 09:32:30 +0800

    android obj folder

--- android/proj.android/src/com/tringame/pocketdungeon/PocketDungeon.java
+++ android/proj.android/src/com/tringame/pocketdungeon/PocketDungeon.java
@@ -23,22 +23,27 @@ THE SOFTWARE.
 ****************************************************************************/
 package com.tringame.pocketdungeon;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.StreamCorruptedException;
 import java.util.UUID;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings.Secure;
 import android.telephony.TelephonyManager;
 
 public class PocketDungeon extends Cocos2dxActivity{
@@ -59,10 +64,7 @@ public class PocketDungeon extends Cocos2dxActivity{
 		try {
 			info = this.getPackageManager().getPackageInfo(getPackageName(), 0);
 			return info.versionName;
-		} catch (NameNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} catch (NameNotFoundException e) {}
   		return "0";
   	}
   	
@@ -90,40 +92,70 @@ public class PocketDungeon extends Cocos2dxActivity{
 						ooutStream.writeUTF(id);
 						ooutStream.close();
 						outStream.close();
-					} catch (IOException e1) {
-						//Nothing
-					}
+					} catch (IOException e1) {}
 					
-				} catch (FileNotFoundException e1) {
-					//create the file if it doesn't already exist
-				}
+				} catch (FileNotFoundException e1) {}
 			}
   		}
   		return id;
   	}
   	
   	//alert
-  	public void alert(String title, String message){
-  		// TODO
+  	public void alert(String title, String message, String... buttons){
+  		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+  		builder.setTitle(title);
+  		builder.setMessage(message);
+  		DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Do something
+				
+			}
+		};
+  		for(String s : buttons){
+  			builder.setNeutralButton(s, listener);
+  		}
+  		builder.show();
   	}
   	
   	//open URL
   	public void openURL(String url){
-  		
+  		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+  		startActivity(browserIntent);
   	}
   	
   	//check Network Status
   	public int checkNetworkStatus(){
-  		return 0;
+  		ConnectivityManager conmgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+  		NetworkInfo info = conmgr.getActiveNetworkInfo();
+  		if( info != null && info.isConnected() ){
+  			return 1;
+  		}
+  		else{
+  			return 0;
+  		}
   	}
   	
   	//create directory at path
   	public boolean createDirectoryAtPath(String path){
+  		File folder = new File(path);
+  		if( !folder.exists() ){
+  			folder.mkdirs();
+  		}
   		return true;
   	}
   	
   	//remove directory
   	public boolean removeDirectory(String path){
+  		File folder = new File(path);
+  		if( folder.exists() ){
+  			if(folder.isDirectory()){
+  				for(String item : folder.list()){
+  					new File(path, item).delete();
+  				}
+  			}
+  			folder.delete();
+  		}
   		return true;
   	}
 }
