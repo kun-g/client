17a9df5b6476a6fee74a24e00400d242b7f5c482
Jovi Du <ijovi23@gmail.com>
2014-06-20 17:24:58 +0800

    no message

--- Resources/script/sceneMain.js
+++ Resources/script/sceneMain.js
@@ -414,8 +414,8 @@ function onChat(sender)
 function onStage(sender)
 {
     //test
-    requestBattle(124, [engine.user.actor]);
-    return;
+//    requestBattle(124, [engine.user.actor]);
+//    return;
     
     cc.AudioEngine.getInstance().playEffect("card2.mp3");
     startCloseAnimation(function(){
